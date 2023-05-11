baca37c5be0ce50d6ef0149d07d6f57048331957
leiaifce <leia@maranguape.ifce.edu.br>
2023-05-11 10:32:48 -0300

    corrigindo erros

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import connectDatabase from './src/database/db.js'
 import Route from "./src/routes/routes.js"
 import dotenv from "dotenv"
 import cookieParser from 'cookie-parser'
+import cors from 'cors'
 
 
 dotenv.config()
@@ -12,11 +13,13 @@ dotenv.config()
 const app = express()
 const port = 3000
 
+
+
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
 })); 
-
+app.use(cors())
 connectDatabase()
 
 app.use(cookieParser())
